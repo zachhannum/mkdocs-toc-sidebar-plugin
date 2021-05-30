3a281605862cdaaf5cbfe796aa8753410223c84d
GlennNZ <glenn.mccord@gmail.com>
2021-05-30 21:11:43 +1200

    Update plugin.py
    Fixed compatibility issue with mkdocs 1.1 which resulted in a runtime error: AttributeError: module 'mkdocs.utils' has no attribute 'string_types'

--- mkdocs_toc_sidebar_plugin/plugin.py
+++ mkdocs_toc_sidebar_plugin/plugin.py
@@ -3,7 +3,6 @@ import sys
 from timeit import default_timer as timer
 from datetime import datetime, timedelta
 
-from mkdocs import utils as mkdocs_utils
 from mkdocs.config import config_options, Config
 from mkdocs.plugins import BasePlugin
 
@@ -14,7 +13,7 @@ import codecs
 class TocSidebar(BasePlugin):
 
     config_scheme = (
-        ('param', config_options.Type(mkdocs_utils.string_types, default='')),
+        ('param', config_options.Type(str, default='')),
     )
 
     def __init__(self):
